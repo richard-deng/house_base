c32012d5ccd0b6fd4d8768377c557ab99420ff61
邓成 <dengcheng@qfpay.com>
2018-05-09 13:59:23 +0800

    修改

--- box_list.py
+++ box_list.py
@@ -55,7 +55,7 @@ class BoxList:
     def load_all(cls):
         where = {'available': define.BOX_ENABLE}
         other = ' order by priority desc '
-        keep_fields = ['id', 'name', 'icon', 'priority', 'available']
+        keep_fields = ['id', 'name', 'icon', 'priority', 'available', 'box_type']
         with get_connection_exception(TOKEN_HOUSE_CORE) as conn:
             ret = conn.select(table=BoxList.TABLE, fields=keep_fields, where=where, other=other)
             return ret
