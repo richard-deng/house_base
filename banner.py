ec3a53e5708bdb8e78558e63755b93eed55c8212
邓成 <dengcheng@qfpay.com>
2018-08-06 21:55:29 +0800

    add

--- banner.py
+++ banner.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import copy
 import logging
+import datetime
 
 import tools
 import define
@@ -36,6 +37,7 @@ class Banners:
         self.id  = banner_id
         self.data = {}
         self.keys = {}
+        self.load()
 
     def load(self):
         where = {'id': self.id}
@@ -65,4 +67,52 @@ class Banners:
             for record in records:
                 cls.to_string(record)
             return records
+
+    @classmethod
+    def create(cls, values):
+        now = datetime.datetime.now()
+        now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+        values['ctime'] = now_str
+        values['utime'] = now_str
+        with get_connection_exception(TOKEN_HOUSE_CORE) as conn:
+            ret = conn.insert(table=cls.TABLE, values=values)
+            return ret
+
+    @classmethod
+    def load_by_title(cls, title):
+        where = {'title': title}
+        with get_connection_exception(TOKEN_HOUSE_CORE) as conn:
+            ret = conn.select_one(table=cls.TABLE, where=where)
+            return ret
+
+    def update(self, values):
+        where = {'id': self.id}
+        now = datetime.datetime.now()
+        now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+        values.update({'utime': now_str})
+        with get_connection_exception(TOKEN_HOUSE_CORE) as conn:
+            ret = conn.update(table=self.TABLE, values=values, where=where)
+            return ret
+
+
+    @classmethod
+    def page(cls, **kwargs):
+        need_query = cls.QUERY_KEY.keys()
+        where = {}
+        for k, v in kwargs.iteritems():
+            if k in need_query and kwargs.get(k):
+                where[k] = kwargs.get(k)
+        other = kwargs.get('other', ' order by ctime desc ')
+        page = kwargs.get('page', 1)
+        page_size = kwargs.get('maxnum', 10)
+        log.debug('BOX_LIST_KEY=%s', cls.KEYS)
+        keep_fields = copy.deepcopy(cls.KEYS)
+        if cls.TABLE_ID not in keep_fields:
+            keep_fields.append(cls.TABLE_ID)
+        log.debug('keep_fields=%s', keep_fields)
+        with get_connection_exception(TOKEN_HOUSE_CORE) as conn:
+            sql = conn.select_sql(table=cls.TABLE, where=where, fields=keep_fields, other=other)
+            pager = conn.select_page(sql, pagecur=page, pagesize=page_size)
+            pager.split()
+            return pager.pagedata.data, pager.count
         
